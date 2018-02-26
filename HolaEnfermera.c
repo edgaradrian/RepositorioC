44359426eeec8a2a850e97eb32ada472aa32d165
Edgar <addick@ADRiMac.local>
2018-02-25 22:44:28 -0600

    Mi primer programa en C

---
+++ HolaEnfermera.c
@@ -0,0 +1,13 @@
+//
+//  HolaEnfermera.c
+//  
+//
+//  Created by Edgar on 25/02/18.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])  {
+    printf("Holaaaaaa... Enfermera");
+    return 0;
+}//main
